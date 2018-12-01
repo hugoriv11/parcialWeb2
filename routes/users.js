371ca7c6369223fda3c55ba17ba4499d77d4ba67
hugoriv11 <00161417@uca.edu.sv>
2018-12-01 16:43:23 -0600

    haciendo rutas del crud y haciendo formularios basicos

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+const imanController = require('../controllers/ImanControllers');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/', imanController.index);
+
+router.get('/:id', imanController.findIman);
+
+router.post('/', imanController.create);
+
+router.put('/:id', imanController.update);
+
+router.delete('/:id', imanController.delete);
 
 module.exports = router;
